2c0e90f9e3a447e623f1c0e501804825c6136e8d
Marshall Davis <toothlessrebel@gmail.com>
2016-08-21 22:49:32 -0400

    Adds Travis testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
         Designed to provide custom, rule-enforced character sheets.
     </div>
     <div class="row">
-        Also provides and API for accessing information about rule sets.
+        Also provides and API for accessing information about rule sets. The API tests are currently:
+        <img src="https://travis-ci.org/ToothlessRebel/character_sheets.svg?branch=master" alt="Build Status Badge">
     </div>
 @endsection
