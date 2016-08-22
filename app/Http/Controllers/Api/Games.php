d6ec47f66c31f1512f6215bb01f7367c3669b773
Marshall Davis <toothlessrebel@gmail.com>
2016-08-21 23:43:58 -0400

    Makes first API route accessible

--- app/Http/Controllers/Api/Games.php
+++ app/Http/Controllers/Api/Games.php
@@ -8,8 +8,29 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Game;
 
 class Games
 {
+    public function index()
+    {
+        $games = Game::all(['id', 'name'])->toArray();
 
+        return response()->json($games);
+    }
+
+    public function store()
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function destroy($id)
+    {
+        // TODO: Implement destroy() method.
+    }
+
+    public function show()
+    {
+        // TODO: Implement show() method.
+    }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,4 +11,16 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'Intro@index');
+
+Route::group(
+    [
+        'namespace'  => 'Api',
+        'middleware' => 'tokenAuth',
+    ],
+    function () {
+        Route::get('/api/games/', 'Games@index');
+    }
+);
